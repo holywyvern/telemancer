ead3cb6bd658756742f07f1fd5c4a536d8a22024
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2019-07-28 18:24:56 -0300

    add some teleports

--- game/data/events/case1/entrance.lua
+++ game/data/events/case1/entrance.lua
@@ -1,5 +1,8 @@
 local tv = require("prototypes.events.tvGate")
+local backDoor = require("prototypes.events.backDoor")
 
 return {
-  tv:create('tv', {14, 11}, { map="case1_house", pos={15, 4, 2} })
+  tv:create('tv', {14, 11}, { map="case1_house", pos={15, 4, 2} }),
+  backDoor:create("tp1", {8, 1}, { map="case1/stands", pos={8, 31} }),
+  backDoor:create("tp2", {9, 1}, { map="case1/stands", pos={9, 31} })
 }
--- game/data/events/case1/stands.lua
+++ game/data/events/case1/stands.lua
@@ -1,1 +1,6 @@
-return {}
+local backDoor = require("prototypes.events.backDoor")
+
+return {
+  backDoor:create("tp1", {8, 32}, { map="case1/entrance", pos={8, 2} }),
+  backDoor:create("tp2", {9, 32}, { map="case1/entrance", pos={9, 2} })  
+}
