e1ccbf57d00910d630d3fde1958a5384159d9654
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2019-07-29 00:25:25 -0300

    fix merry go round map

--- game/data/events/case1/merry_go_round.lua
+++ game/data/events/case1/merry_go_round.lua
@@ -1,1 +1,6 @@
-return {}
+local backDoor = require("prototypes.events.backDoor")
+
+return {
+  backDoor:create("tp1", {1, 6}, { map="case1/plaza", pos={49, 12} }),
+  backDoor:create("tp2", {1, 7}, { map="case1/plaza", pos={49, 13} })  
+}
--- game/data/events/case1/plaza.lua
+++ game/data/events/case1/plaza.lua
@@ -4,5 +4,7 @@ return {
   backDoor:create("mirrors_tp1", {9, 50}, { map="case1/mirrors_out", pos={13, 2} }),
   backDoor:create("mirrors_tp2", {10, 50}, { map="case1/mirrors_out", pos={14, 2} }),
   backDoor:create("stand_tp1", {24, 50}, { map="case1/stands", pos={8, 2} }),
-  backDoor:create("stand_tp2", {25, 50}, { map="case1/stands", pos={9, 2} })
+  backDoor:create("stand_tp2", {25, 50}, { map="case1/stands", pos={9, 2} }),
+  backDoor:create("merry_go_round_tp1", {50, 12}, { map="case1/merry_go_round", pos={2, 6} }),
+  backDoor:create("merry_go_round_tp2", {50, 13}, { map="case1/merry_go_round", pos={2, 7} })
 }
