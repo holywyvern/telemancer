11ab311acb5d0c902b0f1058802397a838ff149b
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2019-08-05 02:06:27 -0300

    remove roadblocks in kid mode

--- game/data/events/case1/tent_in.lua
+++ game/data/events/case1/tent_in.lua
@@ -1,5 +1,5 @@
 local backDoor = require("prototypes.events.backDoor")
 
 return {
-  backDoor:create("in_tp", {17, 32}, { map="case1/tent_in", pos={7, 9} })
+  backDoor:create("in_tp", {17, 32}, { map="case1/tent_out", pos={7, 9} })
 }
