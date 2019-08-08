f2aed9ab4b597cd64012680f761d809edf32e767
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2019-08-08 15:10:53 -0300

    add ridiculously long credit scene base

--- game/scenes/credits.lua
+++ game/scenes/credits.lua
@@ -8,6 +8,25 @@ local fade = require("prototypes.transitions.fadeInOut")
 
 local scene = base:extend()
 
+local txtTitle = "Coding"
+local txtDev = "Ramiro Rojo (RR)"
+local txtDev2 = "Gonzalo Leonel Gutierrez (Guty)"
+local txtTitle2 = "Music"
+local txtMusic = "Ignacio Coluccio (Colulo)"
+local txtTnks = "Special Thanks"
+local thanks = {
+  "Natalia Pastorutti (Nati <3)",
+  "Soledad Pastorutti (Sole)",
+  "Ariel Cid (Acid)",
+  "Mom",
+  "Dad",
+  "Nicolás Zarewsky (Nicolás Zarewsky)",
+  "Franco Prudhomme (Prudi)",
+}
+local speed = 32
+
+local txtYou = "And YOU"
+
 function scene:enter(previous, ...)
   self._t = 0
   self._font = love.graphics.newFont("fonts/game.ttf", 16)
@@ -16,34 +35,30 @@ function scene:enter(previous, ...)
 end
 
 function scene:draw()
-  local scrollY = engine.game.height + 96 - (self._t - 1) * 32
-  local txtTitle = "Coding"
-  local txtDev = "Ramiro Rojo (RR)"
-  local txtDev2 = "Gonzalo Leonel Gutierrez (Guty)"
-  local txtTitle2 = "Music"
-  local txtMusic = "Ignacio Coluccio (Colulo)"
-  local txtTnks = "Special Thanks"
-  local txtNati = "Natalia Pastorutti"
-  local txtAcid = "Ariel Cid (Acid)"
-  local txtYou = "And YOU"
+  local scrollY = engine.game.height + 96 - (self._t - 1) * speed
   local twTitle = self._font:getWidth(txtTitle) + 1
   local twDev = self._font:getWidth(txtDev) + 1
   local twDev2 = self._font:getWidth(txtDev2) + 1
   local twTitle2 = self._font:getWidth(txtTitle2) + 1
   local twMusic = self._font:getWidth(txtMusic) + 1
-  local twTnks = self._font:getWidth(txtTnks) + 1
-  local twNati = self._font:getWidth(txtNati) + 1
+  local twTkT = self._font:getWidth(txtTnks) + 1
   local twYou = self._font:getWidth(txtYou) + 1
-  local twAcid = self._font:getWidth(txtAcid) + 1
+  local twThnks = {}
+  local xThnks = {}
+  local yThnks = {}
+  for i, txt in ipairs(thanks) do
+    w = self._font:getWidth(txt)
+    twThnks[i] = w
+    xThnks[i] = math.floor((engine.game.width - w) / 2)
+    yThnks[i] = 150 + 20 * i
+  end
   local xTitle, yTitle = math.floor((engine.game.width - twTitle) / 2), 30
   local xDev, yDev = math.floor((engine.game.width - twDev) / 2), 50
   local xDev2, yDev2 = math.floor((engine.game.width - twDev2) / 2), 70
   local xTitle2, yTitle2 = math.floor((engine.game.width - twTitle2) / 2), 100
   local xMusic, yMusic = math.floor((engine.game.width - twMusic) / 2), 120
-  local xTnks, yTnks = math.floor((engine.game.width - twTnks) / 2), 150
-  local xNati, yNati = math.floor((engine.game.width - twNati) / 2), 170
-  local xYou, yYou = math.floor((engine.game.width - twYou) / 2), 250
-  local xAcid, yAcid = math.floor((engine.game.width - twAcid) / 2), 190
+  local xTkt, yTkt = math.floor((engine.game.width - twTkT) / 2), 150
+  local xYou, yYou = math.floor((engine.game.width - twYou) / 2), yThnks[#yThnks] + 40
   love.graphics.setFont(self._font)
   love.graphics.setColor(self:titleColor())
   love.graphics.print(txtTitle, xTitle, yTitle + scrollY)
@@ -55,10 +70,11 @@ function scene:draw()
   love.graphics.setColor(self:textColor())
   love.graphics.print(txtMusic, xMusic, yMusic + scrollY)
   love.graphics.setColor(self:titleColor())
-  love.graphics.print(txtTnks, xTnks, yTnks + scrollY)
+  love.graphics.print(txtTnks, xTkt, yTkt + scrollY)
   love.graphics.setColor(self:textColor())
-  love.graphics.print(txtNati, xNati, yNati + scrollY)
-  love.graphics.print(txtAcid, xAcid, yAcid + scrollY)
+  for i, txt in ipairs(thanks) do
+    love.graphics.print(txt, xThnks[i], yThnks[i] + scrollY)  
+  end
   love.graphics.print(txtYou, xYou, yYou + scrollY)
 end
 
@@ -72,10 +88,14 @@ end
 
 function scene:update(dt)
   self._t = self._t + dt
-  if self._t > 20 and not self._do then
+  if self._t > self:maxT() and not self._do then
     manager:switch("title", { transition = fade:create(0.5) })
     self._do = true
   end
+end
+
+function scene:maxT()
+  return (2 * engine.game.height + 20 * (#thanks + 13)) / speed
 end
 
 return scene
