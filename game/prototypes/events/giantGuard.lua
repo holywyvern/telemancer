8daaf8e29de16c8bdec751e95ae2c0ce6a72ca10
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2019-08-08 23:36:14 -0300

    fix guard

--- game/prototypes/events/giantGuard.lua
+++ game/prototypes/events/giantGuard.lua
@@ -9,10 +9,10 @@ function giant:create(position, orientation, viewD)
   local evt = guard.create(self, 'big_clown', position)
   evt._mode = 'Chase'
   evt._d = position[3] or 2
-  evt._speed = 2
+  evt._speed = 3
   evt._orientation = orientation or 'cw'
   evt._viewD = viewD or 6
-  evt._size = 2
+  evt._size = 1
   evt._solid = true
   evt._tactile = true
   evt._imgName = 'case1/big_clown'
