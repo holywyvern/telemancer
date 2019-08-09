af2740cb51150f7ea7c4c2af73833837c3f9eb8e
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2019-08-08 23:42:28 -0300

    fix guards in tent

--- game/data/events/case1/tent_in.lua
+++ game/data/events/case1/tent_in.lua
@@ -5,15 +5,15 @@ local giantGuard = require("prototypes.events.giantGuard")
 
 return {
   backDoor:create("in_tp", {17, 32}, { map="case1/tent_out", pos={7, 9} }),
-  guard:create({13, 15, 4}),
-  guard:create({16, 12, 4}),
-  guard:create({5, 21, 2}),
-  guard:create({28, 15, 8}),
-  guard:create({14, 26, 4}),
-  guard:create({14, 26, 4}),
-  guard:create({13, 12, 4}),
-  guard:create({16, 12, 4}),
-  guard:create({16, 15, 4}),
-  guard:create({14, 10, 4}),
+  guard:create("tent_in_1", {13, 15, 4}),
+  guard:create("tent_in_2", {16, 12, 4}),
+  guard:create("tent_in_3", {5, 21, 2}),
+  guard:create("tent_in_4", {28, 15, 8}),
+  guard:create("tent_in_5", {14, 26, 4}),
+  guard:create("tent_in_6", {14, 26, 4}),
+  guard:create("tent_in_7", {13, 12, 4}),
+  guard:create("tent_in_8", {16, 12, 4}),
+  guard:create("tent_in_9", {16, 15, 4}),
+  guard:create("tent_in_10", {14, 10, 4}),
   giantGuard:create({17, 18, 2})
 }
